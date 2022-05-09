dcbbdb2b131a418fa5c003c0a81db66aca6303f0
Simon Wang <mtwangyugeng@gmail.com>
2022-05-09 15:28:38 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+
+  let imp = "";
+  let neo = "";
+
+  const readyChannel = new BroadcastChannel('ready_channel');
+  readyChannel.postMessage('ready');
+  readyChannel.addEventListener("message", e => {
+    if(e.data === 'ready') {
+      workChannel.postMessage(imp);
+    }
+  });
+
+  const workChannel = new BroadcastChannel('work_channel');
+
+  workChannel.addEventListener("message", e => {
+    neo = e.data;
+  });
+
+  $: workChannel.postMessage(imp);
+
+  
+</script>
+
+<main>
+  {neo}
+  <input bind:value={imp}/>
+  {imp}
+</main>
+
+<style>
+
+</style>
